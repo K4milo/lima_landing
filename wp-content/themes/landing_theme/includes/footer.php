aef4e9a7f6ea51f4a52825709dcd3de5ecce71c1
Camilo Vanegas <kmilo.vanegas@gmail.com>
2018-02-14 10:12:49 -0500

    ajuste facebook

--- wp-content/themes/landing_theme/includes/footer.php
+++ wp-content/themes/landing_theme/includes/footer.php
@@ -27,6 +27,25 @@
 </div>
 
 <?php wp_footer(); ?>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '1848923068742787'); 
+fbq('track', 'PageView');
+</script>
+<noscript>
+ <img height="1" width="1" 
+src="https://www.facebook.com/tr?id=1848923068742787&ev=PageView
+&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112422655-1"></script>
 <script>
